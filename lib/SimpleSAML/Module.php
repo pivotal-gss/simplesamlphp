b370cff085c5bb793dcf5685f92421da63bc6360
Tim van Dijen <tvdijen@gmail.com>
2019-11-16 17:44:08 +0100

    Fix PossiblyNullArrayAccess

--- lib/SimpleSAML/Module.php
+++ lib/SimpleSAML/Module.php
@@ -541,7 +541,7 @@ class Module
                 self::$module_info[$module]['hooks'] = self::getModuleHooks($module);
             }
 
-            if (!isset(self::$module_info[$module]['hooks'][$hook])) {
+            if (!isset(self::$module_info[$module]['hooks'][$hook]) || !empty(self::$module_info[$module]['hooks'][$hook])) {
                 continue;
             }
 
